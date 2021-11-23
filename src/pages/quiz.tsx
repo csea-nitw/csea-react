17f4af875b1bded84f8a482198340830ce8fd341
Divya Soni <ds952073@student.nitw.ac.in>
2021-11-23 12:51:03 +0400

    added modal to images

--- src/pages/quiz.tsx
+++ src/pages/quiz.tsx
@@ -1,7 +1,10 @@
-import { Button, Grid, Typography } from '@mui/material';
+import { Button, Grid, Typography, Stack } from '@mui/material';
 import Box from '@mui/material/Box';
 import React, { useState } from 'react';
 import DoneIcon from '@mui/icons-material/Done';
+import { Modal } from '@mui/material';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import Tooltip from '@mui/material/Tooltip';
 import { quizAvailable } from '../constants/sampleQuizData';
 
 function Quiz() {
@@ -12,10 +15,20 @@ function Quiz() {
   ]);
 
   const getColorForPallete = (index: number, selectedOption: number): string => {
-    if (currentQs === index) return '#5B246C';
     if (selectedOption !== -1) return '#1AA260  ';
+    if (currentQs === index) return '#5B246C';
     return 'white';
   };
+  const getFontColorForPallete = (index: number, selectedOption: number): string => {
+    if (currentQs === index) return 'white';
+    if (selectedOption !== -1) return 'white';
+    return 'black';
+  };
+  // modal
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const style = {};
   return (
     <Grid
       container
@@ -24,6 +37,7 @@ function Quiz() {
       style={{ userSelect: 'none', backgroundColor: 'white', margin: '35px auto' }}
     >
       {/* pallete thing here */}
+
       <Grid
         item
         xs={11}
@@ -34,7 +48,9 @@ function Quiz() {
         direction="column"
         style={{ backgroundColor: '#C4C4C4', borderRadius: '3vh', padding: '2vh' }}
       >
-        <Typography fontWeight={600}>Quiz Pallete</Typography>
+        <Typography variant="h5" fontWeight={600}>
+          Quiz Pallete
+        </Typography>
         <Grid item container direction="row" style={{ justifyContent: 'space-evenly' }}>
           {quizAvailable.questions.map((item, index) => (
             <Grid
@@ -44,20 +60,26 @@ function Quiz() {
               mt={1}
               style={{
                 backgroundColor: getColorForPallete(index, selected[index]),
-                borderRadius: '2vh',
+                borderRadius: '5vh',
               }}
             >
-              {1 + index}
+              <Button
+                disableFocusRipple
+                disableTouchRipple
+                sx={{ color: getFontColorForPallete(index, selected[index]) }}
+              >
+                {1 + index}
+              </Button>
             </Grid>
           ))}
         </Grid>
         <Grid item container direction="row" justifyContent="space-around" alignItems="center">
           <Grid item xs={4}>
             <Button
-              variant="contained"
+              variant="outlined"
               size="small"
               sx={{
-                padding: '5px',
+                padding: '7px',
                 marginTop: '10px',
               }}
               onClick={() => {
@@ -68,31 +90,56 @@ function Quiz() {
             </Button>
           </Grid>
           <Grid item xs={4}>
-            <Button
-              variant="contained"
-              size="small"
-              sx={{
-                padding: '5px',
-                marginTop: '10px',
-              }}
-              onClick={() => {
-                const newArr: number[] = [];
-                selected.forEach((selectedOption, index) => {
-                  if (currentQs === index) {
-                    newArr.push(-1);
-                  } else {
-                    newArr.push(selectedOption);
-                  }
-                });
-                setSelected(newArr);
-              }}
-            >
-              Clear Response
-            </Button>
+            {selected[currentQs] !== -1 ? (
+              <Button
+                variant="outlined"
+                size="small"
+                sx={{
+                  padding: '5px',
+                  marginTop: '10px',
+                }}
+                onClick={() => {
+                  const newArr: number[] = [];
+                  selected.forEach((selectedOption, index) => {
+                    if (currentQs === index) {
+                      newArr.push(-1);
+                    } else {
+                      newArr.push(selectedOption);
+                    }
+                  });
+                  setSelected(newArr);
+                }}
+              >
+                Clear Response
+              </Button>
+            ) : (
+              <Button
+                variant="outlined"
+                size="small"
+                disabled
+                sx={{
+                  padding: '5px',
+                  marginTop: '10px',
+                }}
+                onClick={() => {
+                  const newArr: number[] = [];
+                  selected.forEach((selectedOption, index) => {
+                    if (currentQs === index) {
+                      newArr.push(-1);
+                    } else {
+                      newArr.push(selectedOption);
+                    }
+                  });
+                  setSelected(newArr);
+                }}
+              >
+                Clear Response
+              </Button>
+            )}
           </Grid>
           <Grid item xs={4}>
             <Button
-              variant="contained"
+              variant="outlined"
               size="small"
               sx={{
                 padding: '5px',
@@ -117,21 +164,69 @@ function Quiz() {
         <Typography maxWidth="100%">
           Type - {quizAvailable.questions[currentQs].qsType} <DoneIcon />
         </Typography>
+
+        <Typography fontSize={15} color="gray">
+          Click to Enlarge Images
+        </Typography>
+
         {/* Question */}
         {quizAvailable.questions[currentQs].qsImageUrl !== '' ? (
           <Grid item container direction="row">
             <Grid item md={4} xs={12}>
-              <img
-                src={quizAvailable.questions[currentQs].qsImageUrl}
-                alt={`${quizAvailable.questions[currentQs].qsImageUrl}`}
-                style={{
-                  maxWidth: '100%',
-                  maxHeight: '30vh',
-                  margin: 'auto',
-                  padding: '1vh',
-                  borderRadius: '15px',
-                }}
-              />
+              <Tooltip title="Open Image" placement="top-start">
+                <img
+                  src={quizAvailable.questions[currentQs].qsImageUrl}
+                  alt={`${quizAvailable.questions[currentQs].qsImageUrl}`}
+                  style={{
+                    height: '150px',
+                    width: '150px',
+                    margin: 'auto',
+                    padding: '1vh',
+                    borderRadius: '15px',
+                    objectFit: 'cover',
+                  }}
+                  onClick={handleOpen}
+                />
+              </Tooltip>
+
+              <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+              >
+                <Box
+                  sx={{
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                    width: 600,
+                    bgcolor: 'background.paper',
+                    border: '2px solid #000',
+                    boxShadow: 24,
+                    p: 4,
+                  }}
+                >
+                  <Typography id="modal-modal-title" variant="h6" component="h2">
+                    Question {1 + currentQs}
+                  </Typography>
+                  <img
+                    src={quizAvailable.questions[currentQs].qsImageUrl}
+                    alt={`${quizAvailable.questions[currentQs].qsImageUrl}`}
+                    style={{
+                      height: '300px',
+                      margin: 'auto',
+                      padding: '1vh',
+                      borderRadius: '15px',
+                      objectFit: 'cover',
+                    }}
+                  />
+                  <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                    {quizAvailable.questions[currentQs].qsText}
+                  </Typography>
+                </Box>
+              </Modal>
             </Grid>
             <Grid
               item
@@ -217,7 +312,13 @@ function Quiz() {
                   <img
                     src={option.optionUrl}
                     alt={`${option.optionUrl}`}
-                    style={{ maxWidth: '60%', maxHeight: '20vh', margin: 'auto', padding: '1vh' }}
+                    style={{
+                      height: '75px',
+                      width: '75px',
+                      margin: 'auto',
+                      padding: '1vh',
+                      borderRadius: '15px',
+                    }}
                   />
                 </Grid>
                 <Grid
@@ -229,13 +330,14 @@ function Quiz() {
                   }}
                 >
                   <Typography
-                    border="solid"
-                    textAlign="left"
-                    padding="1vh"
+                    textAlign="center"
+                    padding="1.7vh"
                     sx={{
+                      backgroundColor: '#C4C4C4',
                       borderRadius: '30px',
                       '&:hover': {
-                        color: 'red',
+                        fontWeight: 'bold',
+                        cursor: 'pointer',
                       },
                     }}
                     style={
@@ -243,6 +345,7 @@ function Quiz() {
                         ? { backgroundColor: '#1AA260', fontWeight: 'bold' }
                         : {}
                     }
+                    m={1}
                   >
                     {' '}
                     {option.optionText}
